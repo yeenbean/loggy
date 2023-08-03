9d04e88c0dea6fe82f1ca80acfeb9e9d7e1c6931
yeenbean <yeenimal@pm.me>
2023-08-02 17:10:22 -0700

    Initialize repository with `deno init` and add basic logging functions.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # prettier-deno
+
 A simple console logging library for deno.
+
+**This repository exists to allow me to learn how to make modules with deno. I
+do not expect anyone to use it but if you do...let me know! 😉**
+
+## Usage
+
+tbd
---
+++ main.ts
@@ -0,0 +1,131 @@
+// internal functions
+/**
+ * Gets the current time, converts it to a string, and formats it for use in the
+ * console log.
+ * @returns Current time as a string in HH:mm format.
+ */
+function getTimeAsString(): string {
+    // set vars
+    const date = new Date();
+    let hour = date.getHours().toString();
+    let minute = date.getMinutes().toString();
+
+    // format hour and minute if needed
+    if (hour.length == 1) hour = "0" + hour;
+    if (minute.length == 1) minute = "0" + minute;
+
+    // return formatted string
+    return `${hour}:${minute}`;
+}
+
+/**
+ * Logs a message to the console. Prepends the current time.
+ * The status message is three dots with no colored text.
+ * @param message Message to log to the console.
+ */
+export function log(message: string): void {
+    console.log(`[ ... ] ${getTimeAsString()}: ${message}`);
+}
+
+/**
+ * Logs a message to the console. Prepends the current time.
+ * The status message uses three "+" symbols to indicate positive change.
+ * @param message Message to log to the console.
+ */
+export function success(message: string): void {
+    console.log(
+        `[ %c+++%c ] ${getTimeAsString()}: ${message}`,
+        "color: green",
+        "all: unset",
+    );
+}
+
+/**
+ * Logs a message to the console. Prepends the current time.
+ * The status message uses three "-" symbols to indicate negative change.
+ * @param message Message to log to the console.
+ */
+export function fail(message: string): void {
+    console.log(
+        `[ %c---%c ] ${getTimeAsString()}: ${message}`,
+        "color: red",
+        "all: unset",
+    );
+}
+
+/**
+ * Logs a message to the console. Prepends the current time.
+ * The status message uses "NFO" to indicate a warning message.
+ * @param message Message to log to the console.
+ */
+export function info(message: string): void {
+    console.log(
+        `[ %cNFO%c ] ${getTimeAsString()}: ${message}`,
+        "color: yellow",
+        "all: unset",
+    );
+}
+
+/**
+ * Logs a message to the console. Prepends the current time.
+ * The status message uses "WRN" to indicate a warning message.
+ * @param message Message to log to the console.
+ */
+export function warn(message: string): void {
+    console.log(
+        `[ %cWRN%c ] ${getTimeAsString()}: ${message}`,
+        "color: orange",
+        "all: unset",
+    );
+}
+
+/**
+ * Logs a message to the console. Prepends the current time.
+ * The status message uses three "!" to indicate a critical error.
+ * @param message Message to log to the console.
+ */
+export function critical(message: string): void {
+    console.log(
+        `[ %c!!!%c ] ${getTimeAsString()}: ${message}`,
+        "color: red",
+        "all: unset",
+    );
+}
+
+/**
+ * Logs a message to the console. Prepends the current time.
+ * The status message uses three "↑" with a blue hue.
+ * @param message Message to log to the console.
+ */
+export function up(message: string): void {
+    console.log(
+        `[ %c↑↑↑%c ] ${getTimeAsString()}: ${message}`,
+        "color: blue",
+        "all: unset",
+    );
+}
+
+/**
+ * Logs a message to the console. Prepends the current time.
+ * The status message uses three "↓" with a blue hue.
+ * @param message Message to log to the console.
+ */
+export function down(message: string): void {
+    console.log(
+        `[ %c↓↓↓%c ] ${getTimeAsString()}: ${message}`,
+        "color: blue",
+        "all: unset",
+    );
+}
+
+// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+    log("log()");
+    success("success()");
+    fail("fail()");
+    info("info()");
+    warn("warn()");
+    critical("critical()");
+    up("up()");
+    down("down()");
+}
