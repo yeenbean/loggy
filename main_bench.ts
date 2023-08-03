53f385e7e000e1ca21839bb3cc421f7ee5d88a43
yeenbean <yeenimal@pm.me>
2023-08-02 17:26:10 -0700

    Updated tests

--- main_bench.ts
+++ main_bench.ts
@@ -1,9 +1,57 @@
-import { add } from "./main.ts";
+import * as Loggy from "./main.ts";
 
-Deno.bench(function addSmall() {
-    add(1, 2);
+Deno.bench({
+    name: "Standard log",
+    fn() {
+        Loggy.log("Benchmarking...");
+    },
 });
 
-Deno.bench(function addBig() {
-    add(2 ** 32, 2 ** 32);
+Deno.bench({
+    name: "Success log",
+    fn() {
+        Loggy.success("Benchmarking...");
+    },
+});
+
+Deno.bench({
+    name: "Failure log",
+    fn() {
+        Loggy.fail("Benchmarking...");
+    },
+});
+
+Deno.bench({
+    name: "Info log",
+    fn() {
+        Loggy.info("Benchmarking...");
+    },
+});
+
+Deno.bench({
+    name: "Warning log",
+    fn() {
+        Loggy.warn("Benchmarking...");
+    },
+});
+
+Deno.bench({
+    name: "Critical log",
+    fn() {
+        Loggy.critical("Benchmarking...");
+    },
+});
+
+Deno.bench({
+    name: "Up log",
+    fn() {
+        Loggy.up("Benchmarking...");
+    },
+});
+
+Deno.bench({
+    name: "Down log",
+    fn() {
+        Loggy.down("Benchmarking...");
+    },
 });
